9c5853089a04cc028b26c2f26bb4755b6a42bc73
AleUABCS <alr3_23@alu.uabcs.mx>
2025-05-19 17:58:44 -0600

    Update LoginController.java
    Controlador del login con método de autentificación

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import Model.AuthModel;
+import View.View_loginGYM;
+
+public class LoginController {
+	
+	AuthModel model;
+	View_loginGYM view;
+	
+	public LoginController (View_loginGYM view, AuthModel model) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public boolean auth (String email, String password) {
+		return model.auth(email, password);
+	}
+}
