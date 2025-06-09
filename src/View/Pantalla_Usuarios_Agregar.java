be81fc47638972ffef98bb6513b9cd7ea36232bb
IanKarel15 <iankareld_23@alu.uabcs.mx>
2025-06-08 21:58:17 -0700

    redireccion cuando se crea user

--- src/View/Pantalla_Usuarios_Agregar.java
+++ src/View/Pantalla_Usuarios_Agregar.java
@@ -10,6 +10,7 @@ import Funciones_graficas.Graficos_texto;
 import Funciones_graficas.Menu;
 import Model.ClassModel;
 import Model.PaymentModel;
+import Model.User;
 import Model.UserModel;
 
 public class Pantalla_Usuarios_Agregar {
@@ -135,34 +136,46 @@ public class Pantalla_Usuarios_Agregar {
         crear.setFocusPainted(false);
         panel_agg.add(crear);
         crear.addActionListener(e -> {
-        	String nombreUsuario = nombre.getText();
-        	String telUsuario = tel.getText();
-        	String correoUsuario = correo.getText();
-        	String apellidoUsuario = apellido.getText();
-        	
-        	int error = controlador.createUser(nombreUsuario, apellidoUsuario, telUsuario);
-        	
-        	switch(error) {
-        	case 1:
-        		
-        		JOptionPane.showMessageDialog(null, "Rellena todos los campos");
-        		break;
-        		
-        	case 2:
-        		
-        		JOptionPane.showMessageDialog(null, "Nombre o apellido con numeros");
-        		break;
-        	case 3:
-        		JOptionPane.showMessageDialog(null, "Telefono con letras");
-        		break;
-        	case 0:
-        		JOptionPane.showMessageDialog(null, "¡Usuario agregado con éxito!");
-        		break;
-        	}
+    	    String nomU  = nombre.getText();
+    	    String apeU  = apellido.getText();
+    	    String telU  = tel.getText();
+    	    String corrU = correo.getText(); 
 
-        	
-        });
-        
+    	    int controlNum = controlador.createUser(nomU, apeU, telU);
+
+    	    switch (controlNum) {
+    	        case 0:
+    	            JOptionPane.showMessageDialog(menu,
+    	                "Ocurrió un error al acceder a la base de datos.",
+    	                "Error", JOptionPane.ERROR_MESSAGE);
+    	            break;
+    	        case 1:
+    	            JOptionPane.showMessageDialog(menu,
+    	                "Rellena todos los campos.",
+    	                "Datos incompletos", JOptionPane.WARNING_MESSAGE);
+    	            break;
+    	        case 2:
+    	            JOptionPane.showMessageDialog(menu,
+    	                "El nombre y apellido no pueden contener números.",
+    	                "Datos inválidos", JOptionPane.WARNING_MESSAGE);
+    	            break;
+    	        case 3:
+    	            JOptionPane.showMessageDialog(menu,
+    	                "El teléfono no puede contener letras.",
+    	                "Datos inválidos", JOptionPane.WARNING_MESSAGE);
+    	            break;
+    	        default:
+    	            User user = controlador.getUser(controlNum);
+    	            if (user != null) {
+    	                menu_inicio.pintar_vista(new Detalles(menu_inicio, user).getPanel());
+    	            } else {
+    	                JOptionPane.showMessageDialog(menu,
+    	                    "No se pudo recuperar al usuario recién creado.",
+    	                    "Error", JOptionPane.ERROR_MESSAGE);
+    	            }
+    	            break;
+    	    }
+    	});
         cancelar = new JButton("Cancelar");
         cancelar.setBounds(50, 450, 390, 40);
         cancelar.setFont(new Font("Arial", Font.BOLD, 22));
