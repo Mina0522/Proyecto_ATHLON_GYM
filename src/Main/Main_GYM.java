a619a01ab763bca7818bfdb326cf71712ab4d078
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-06-11 03:43:08 -0700

    fix en adaptar los colores en mac y fix en info

--- src/Main/Main_GYM.java
+++ src/Main/Main_GYM.java
@@ -1,14 +1,22 @@
 package Main;
 
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import Model.MyConnection;
 import View.Vista_GYM;
 
 public class Main_GYM {
-	
-	public static void main(String[] args) {
-		
-		MyConnection.connect(); //Iniciar conexión a la base de datos
-		new Vista_GYM(); //Iniciar vista principal
-	}
 
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        MyConnection.connect();  //Iniciar conexión a la base de datos
+
+        new Vista_GYM(); //Iniciar vista principal
+    }
 }
